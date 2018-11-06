135c3bf254a24db505bb6ed6bcd78cefd7874207
shiotomo <t.shiota16@gmail.com>
2018-11-06 21:51:02 +0900

    コントローラを設置した

---
+++ src/main/java/work/tomosse/TkyBrothersScoreServerApplication.java
@@ -0,0 +1,12 @@
+package work.tomosse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TkyBrothersScoreServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TkyBrothersScoreServerApplication.class, args);
+	}
+}
---
+++ src/main/java/work/tomosse/controller/DashboardController.java
@@ -0,0 +1,18 @@
+package work.tomosse.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class DashboardController {
+
+    @GetMapping
+    public ModelAndView index(ModelAndView mav) {
+        mav.setViewName("dashboard/index");
+        return mav;
+    }
+
+}
